c464a6975cc4f6bf79b4e8765050f4a4a43619b2
clear-zhao <117507524+clear-zhao@users.noreply.github.com>
2025-09-12 10:20:23 +0800

    Update Employee.cs

--- aspnetapp/Models/Employee.cs
+++ aspnetapp/Models/Employee.cs
@@ -5,6 +5,8 @@ namespace aspnetapp
         public string ID { get; set; }
         public string Name { get; set; } = null!;
         public int Jurisdiction { get; set; } // 权限
+        public string Identity {get;set; } // 身份
     }
 
 }
+
