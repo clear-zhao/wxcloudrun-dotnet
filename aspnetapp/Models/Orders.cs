186df2b8a8ae010e1207dd1bff99a96cd329f5ee
clear-zhao <117507524+clear-zhao@users.noreply.github.com>
2025-10-10 13:52:26 +0800

    Create Orders.cs

---
+++ aspnetapp/Models/Orders.cs
@@ -0,0 +1,17 @@
+namespace aspnetapp
+{
+    public class Order
+    {
+        public string OrderID { get; set; }
+        public string State { get; set; }
+        public string Name { get; set; }
+        public string WorkID { get; set; }
+        public string ProductNumber { get; set; }
+        public string ToolNumber { get; set; }
+        public string DZSpecifications { get; set; }
+        public string DXSpecifications { get; set; }
+        public string StandardValue { get; set; }
+        public string FirstStandardValue { get; set; }
+        public string Worker { get; set; }
+    }
+}
